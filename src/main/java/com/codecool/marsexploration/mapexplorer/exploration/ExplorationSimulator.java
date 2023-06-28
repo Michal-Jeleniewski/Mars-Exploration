3feff2effc5d72fc25083a93e79951332a7b8bad
dmrozik87 <dmrozik87@gmail.com>
2023-06-28 13:33:20 +0200

    Removed copy of ColonizationSimulation class

--- src/main/java/com/codecool/marsexploration/mapexplorer/exploration/ExplorationSimulator.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/exploration/ExplorationSimulator.java
@@ -1,23 +1,15 @@
 package com.codecool.marsexploration.mapexplorer.exploration;
 
 import com.codecool.marsexploration.mapexplorer.analizer.AllOutcomeAnalyzer;
-import com.codecool.marsexploration.mapexplorer.commandCenter.CommandCenter;
 import com.codecool.marsexploration.mapexplorer.configuration.ConfigurationParameters;
 import com.codecool.marsexploration.mapexplorer.logger.Logger;
 import com.codecool.marsexploration.mapexplorer.maploader.MapLoader;
-import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
-import com.codecool.marsexploration.mapexplorer.maploader.model.Symbol;
 import com.codecool.marsexploration.mapexplorer.repository.ExplorationsDto;
 import com.codecool.marsexploration.mapexplorer.repository.ExplorationsRepository;
 import com.codecool.marsexploration.mapexplorer.rovers.Rover;
-import com.codecool.marsexploration.mapexplorer.rovers.RoverStatus;
-import com.codecool.marsexploration.mapexplorer.service.CoordinateCalculatorService;
 
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
-
-import static com.codecool.marsexploration.mapexplorer.rovers.RoverStatus.*;
 
 public class ExplorationSimulator {
 
